1a40ce16189793760e8ff1af1c0baf3a1eaa7dfb
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2024-02-06 17:43:19 -0300

    Inital Commit

---
+++ src/app/( reach )/about/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis eaque rem
+      voluptas officiis corrupti saepe eos a dolorum neque magni consectetur,
+      voluptatem totam, eligendi, placeat reprehenderit. Nam enim debitis
+      ratione?
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis eaque
+        rem voluptas officiis corrupti saepe eos a dolorum neque magni
+        consectetur, voluptatem totam, eligendi, placeat reprehenderit. Nam enim
+        debitis ratione?
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Test() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis eaque rem
+      voluptas officiis corrupti saepe eos a dolorum neque magni consectetur,
+      voluptatem totam, eligendi, placeat reprehenderit. Nam enim debitis
+      ratione?
+    </div>
+  );
+}
+
+export default Test;
